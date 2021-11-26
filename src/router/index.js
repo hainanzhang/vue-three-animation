40a5b2cd15ced8d2907358a96633ca884ff9e0fb
zhangchangguo <1379114708@qq.com>
2021-11-26 15:32:37 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,160 +1,125 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-Vue.use(Router)
-
-/* Layout */
-import Layout from '@/layout'
-
-/**
- * Note: 路由配置项
- *
- * hidden: true                     // 当设置 true 的时候该路由不会再侧边栏出现 如401，login等页面，或者如一些编辑页面/edit/1
- * alwaysShow: true                 // 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式--如组件页面
- *                                  // 只有一个时，会将那个子路由当做根路由显示在侧边栏--如引导页面
- *                                  // 若你想不管路由下面的 children 声明的个数都显示你的根路由
- *                                  // 你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
- * redirect: noRedirect             // 当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
- * name:'router-name'               // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
- * query: '{"id": 1, "name": "ry"}' // 访问路由的默认传递参数
- * meta : {
-    noCache: true                   // 如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
-    title: 'title'                  // 设置该路由在侧边栏和面包屑中展示的名字
-    icon: 'svg-name'                // 设置该路由的图标，对应路径src/assets/icons/svg
-    breadcrumb: false               // 如果设置为false，则不会在breadcrumb面包屑中显示
-    activeMenu: '/system/user'      // 当路由设置了该属性，则会高亮相对应的侧边栏。
-  }
- */
-
-// 公共路由
-export const constantRoutes = [
-  {
-    path: '/redirect',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: '/redirect/:path(.*)',
-        component: (resolve) => require(['@/views/redirect'], resolve)
+const router = createRouter({
+  history: createWebHashHistory(), // hash模式：createWebHashHistory，history模式：createWebHistory
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      meta: {
+        index: 1
       }
-    ]
-  },
-  {
-    path: '/login',
-    component: (resolve) => require(['@/views/login'], resolve),
-    hidden: true
-  },
-  {
-    path: '/register',
-    component: (resolve) => require(['@/views/register'], resolve),
-    hidden: true
-  },
-  {
-    path: '/404',
-    component: (resolve) => require(['@/views/error/404'], resolve),
-    hidden: true
-  },
-  {
-    path: '/401',
-    component: (resolve) => require(['@/views/error/401'], resolve),
-    hidden: true
-  },
-  {
-    path: '',
-    component: Layout,
-    redirect: 'index',
-    children: [
-      {
-        path: 'index',
-        component: (resolve) => require(['@/views/index'], resolve),
-        name: 'Index',
-        meta: { title: '首页', icon: 'dashboard', affix: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue'),
+      meta: {
+        index: 1
       }
-    ]
-  },
-  {
-    path: '/user',
-    component: Layout,
-    hidden: true,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'profile',
-        component: (resolve) => require(['@/views/system/user/profile/index'], resolve),
-        name: 'Profile',
-        meta: { title: '个人中心', icon: 'user' }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+      meta: {
+        index: 2
       }
-    ]
-  },
-  {
-    path: '/system/user-auth',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: 'role/:userId(\\d+)',
-        component: (resolve) => require(['@/views/system/user/authRole'], resolve),
-        name: 'AuthRole',
-        meta: { title: '分配角色', activeMenu: '/system/user' }
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: () => import(/* webpackChunkName: "category" */ '@/views/Category.vue'),
+      meta: {
+        index: 1
       }
-    ]
-  },
-  {
-    path: '/system/role-auth',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: 'user/:roleId(\\d+)',
-        component: (resolve) => require(['@/views/system/role/authUser'], resolve),
-        name: 'AuthUser',
-        meta: { title: '分配用户', activeMenu: '/system/role' }
+    },
+    {
+      path: '/product-list',
+      name: 'product-list',
+      component: () => import(/* webpackChunkName: "product-list" */ '@/views/ProductList.vue'),
+      meta: {
+        index: 2
       }
-    ]
-  },
-  {
-    path: '/system/dict-data',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: 'index/:dictId(\\d+)',
-        component: (resolve) => require(['@/views/system/dict/data'], resolve),
-        name: 'Data',
-        meta: { title: '字典数据', activeMenu: '/system/dict' }
+    },
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: () => import(/* webpackChunkName: "product" */ '@/views/ProductDetail.vue'),
+      meta: {
+        index: 3
       }
-    ]
-  },
-  {
-    path: '/monitor/job-log',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: 'index',
-        component: (resolve) => require(['@/views/monitor/job/log'], resolve),
-        name: 'JobLog',
-        meta: { title: '调度日志', activeMenu: '/monitor/job' }
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import(/* webpackChunkName: "cart" */ '@/views/Cart.vue'),
+      meta: {
+        index: 1
       }
-    ]
-  },
-  {
-    path: '/tool/gen-edit',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: 'index',
-        component: (resolve) => require(['@/views/tool/gen/editTable'], resolve),
-        name: 'GenEdit',
-        meta: { title: '修改生成配置', activeMenu: '/tool/gen' }
+    },
+    {
+      path: '/create-order',
+      name: 'create-order',
+      component: () => import(/* webpackChunkName: "create-order" */ '@/views/CreateOrder.vue'),
+      meta: {
+        index: 2
       }
-    ]
-  }
-]
-
-export default new Router({
-  mode: 'history', // 去掉url中的#
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import(/* webpackChunkName: "order" */ '@/views/Order.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/order-detail',
+      name: 'order-detail',
+      component: () => import(/* webpackChunkName: "order-detail" */ '@/views/OrderDetail.vue'),
+      meta: {
+        index: 3
+      }
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: () => import(/* webpackChunkName: "user" */ '@/views/User.vue'),
+      meta: {
+        index: 1
+      }
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: () => import(/* webpackChunkName: "setting" */ '@/views/Setting.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/address',
+      name: 'address',
+      component: () => import(/* webpackChunkName: "address" */ '@/views/Address.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/address-edit',
+      name: 'address-edit',
+      component: () => import(/* webpackChunkName: "address-edit" */ '@/views/AddressEdit.vue'),
+      meta: {
+        index: 3
+      }
+    },
+  ]
 })
+
+export default router
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,219 +1,221 @@
+<!--
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
+ * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
+ * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
+ * Copyright (c) 2020 陈尼克 all rights reserved.
+ * 版权所有，侵权必究！
+ *
+-->
+
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
-      <el-form-item prop="username">
-        <el-input
-          v-model="loginForm.username"
-          type="text"
-          auto-complete="off"
-          placeholder="账号"
-        >
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
+    <s-header :name="type == 'login' ? '登录' : '注册'" :back="'/home'"></s-header>
+    <img class="logo" src="https://s.yezgea02.com/1604045825972/newbee-mall-vue3-app-logo.png" alt="">
+    <div v-if="type == 'login'" class="login-body login">
+      <van-form @submit="onSubmit">
+        <van-field
+          v-model="username"
+          name="username"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+          v-model="password"
           type="password"
-          auto-complete="off"
+          name="password"
+          label="密码"
           placeholder="密码"
-          @keyup.enter.native="handleLogin"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+        <van-field
+          center
+          clearable
+          label="验证码"
+          placeholder="输入验证码"
+          v-model="verify"
         >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="code" v-if="captchaOnOff">
-        <el-input
-          v-model="loginForm.code"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+          <template #button>
+            <vue-img-verify ref="verifyRef" />
+          </template>
+        </van-field>
+        <div style="margin: 16px;">
+          <div class="link-register" @click="toggle('register')">立即注册</div>
+          <van-button round block color="#1baeae" native-type="submit">登录</van-button>
         </div>
-      </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
-        <el-button
-          :loading="loading"
-          size="medium"
-          type="primary"
-          style="width:100%;"
-          @click.native.prevent="handleLogin"
+      </van-form>
+    </div>
+    <div v-else class="login-body register">
+      <van-form @submit="onSubmit">
+        <van-field
+          v-model="username1"
+          name="username1"
+          label="用户名"
+          placeholder="用户名"
+          :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+          v-model="password1"
+          type="password"
+          name="password1"
+          label="密码"
+          placeholder="密码"
+          :rules="[{ required: true, message: '请填写密码' }]"
+        />
+        <van-field
+          center
+          clearable
+          label="验证码"
+          placeholder="输入验证码"
+          v-model="verify"
         >
-          <span v-if="!loading">登 录</span>
-          <span v-else>登 录 中...</span>
-        </el-button>
-        <div style="float: right;" v-if="register">
-          <router-link class="link-type" :to="'/register'">立即注册</router-link>
+          <template #button>
+            <vue-img-verify ref="verifyRef" />
+          </template>
+        </van-field>
+        <div style="margin: 16px;">
+          <div class="link-login" @click="toggle('login')">已有登录账号</div>
+          <van-button round block color="#1baeae" native-type="submit">注册</van-button>
         </div>
-      </el-form-item>
-    </el-form>
-    <!--  底部  -->
-    <div class="el-login-footer">
-      <span>Copyright © 2018-2021 ruoyi.vip All Rights Reserved.</span>
+      </van-form>
     </div>
   </div>
 </template>
 
 <script>
-import { getCodeImg } from "@/api/login";
-import Cookies from "js-cookie";
-import { encrypt, decrypt } from '@/utils/jsencrypt'
-
+import { reactive, ref, toRefs } from 'vue'
+import sHeader from '@/components/SimpleHeader'
+import vueImgVerify from '@/components/VueImageVerify'
+import { login, register } from '@/service/user'
+import { setLocal } from '@/common/js/utils'
+import md5 from 'js-md5'
+import { Toast } from 'vant'
 export default {
-  name: "Login",
-  data() {
+  setup() {
+    const verifyRef = ref(null)
+    const state = reactive({
+      username: '',
+      password: '',
+      username1: '',
+      password1: '',
+      type: 'login',
+      imgCode: '',
+      verify: ''
+    })
+
+    // 切换登录和注册两种模式
+    const toggle = (v) => {
+      state.type = v
+      state.verify = ''
+    }
+
+    // 提交登录或注册表单
+    const onSubmit = async (values) => {
+      console.log('verifyRef.value.imgCode', verifyRef.value.imgCode)
+      state.imgCode = verifyRef.value.imgCode || ''
+      if (state.verify.toLowerCase() != state.imgCode.toLowerCase()) {
+        Toast.fail('验证码有误')
+        return
+      }
+      if (state.type == 'login') {
+        const { data } = await login({
+          "loginName": values.username,
+          "passwordMd5": md5(values.password)
+        })
+        setLocal('token', data)
+        // 需要刷新页面，否则 axios.js 文件里的 token 不会被重置
+        window.location.href = '/'
+      } else {
+        await register({
+          "loginName": values.username1,
+          "password": values.password1
+        })
+        Toast.success('注册成功')
+        state.type = 'login'
+        state.verify = ''
+      }
+    }
+
     return {
-      codeUrl: "",
-      loginForm: {
-        username: "admin",
-        password: "admin123",
-        rememberMe: false,
-        code: "",
-        uuid: ""
-      },
-      loginRules: {
-        username: [
-          { required: true, trigger: "blur", message: "请输入您的账号" }
-        ],
-        password: [
-          { required: true, trigger: "blur", message: "请输入您的密码" }
-        ],
-        code: [{ required: true, trigger: "change", message: "请输入验证码" }]
-      },
-      loading: false,
-      // 验证码开关
-      captchaOnOff: true,
-      // 注册开关
-      register: false,
-      redirect: undefined
-    };
-  },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect;
-      },
-      immediate: true
+      ...toRefs(state),
+      toggle,
+      onSubmit,
+      verifyRef
     }
   },
-  created() {
-    this.getCode();
-    this.getCookie();
-  },
-  methods: {
-    getCode() {
-      getCodeImg().then(res => {
-        this.captchaOnOff = res.captchaOnOff === undefined ? true : res.captchaOnOff;
-        if (this.captchaOnOff) {
-          this.codeUrl = "data:image/gif;base64," + res.img;
-          this.loginForm.uuid = res.uuid;
-        }
-      });
-    },
-    getCookie() {
-      const username = Cookies.get("username");
-      const password = Cookies.get("password");
-      const rememberMe = Cookies.get('rememberMe')
-      this.loginForm = {
-        username: username === undefined ? this.loginForm.username : username,
-        password: password === undefined ? this.loginForm.password : decrypt(password),
-        rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
-      };
-    },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true;
-          if (this.loginForm.rememberMe) {
-            Cookies.set("username", this.loginForm.username, { expires: 30 });
-            Cookies.set("password", encrypt(this.loginForm.password), { expires: 30 });
-            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 });
-          } else {
-            Cookies.remove("username");
-            Cookies.remove("password");
-            Cookies.remove('rememberMe');
-          }
-          this.$store.dispatch("Login", this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || "/" }).catch(()=>{});
-          }).catch(() => {
-            this.loading = false;
-            if (this.captchaOnOff) {
-              this.getCode();
-            }
-          });
-        }
-      });
-    }
+  components: {
+    sHeader,
+    vueImgVerify
   }
-};
+}
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
-.login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
-  background-size: cover;
-}
-.title {
-  margin: 0px auto 30px auto;
-  text-align: center;
-  color: #707070;
-}
-
-.login-form {
-  border-radius: 6px;
-  background: #ffffff;
-  width: 400px;
-  padding: 25px 25px 5px 25px;
-  .el-input {
-    height: 38px;
-    input {
-      height: 38px;
+<style lang="less">
+  .login {
+    .logo {
+      width: 120px;
+      height: 120px;
+      display: block;
+      margin: 80px auto 20px;
+    }
+    .login-body {
+      padding: 0 20px;
+    }
+    .login {
+      .link-register {
+        font-size: 14px;
+        margin-bottom: 20px;
+        color: #1989fa;
+        display: inline-block;
+      }
+    }
+    .register {
+      .link-login {
+        font-size: 14px;
+        margin-bottom: 20px;
+        color: #1989fa;
+        display: inline-block;
+      }
+    }
+    .verify-bar-area {
+      margin-top: 24px;
+      .verify-left-bar {
+        border-color: #1baeae;
+      }
+      .verify-move-block {
+        background-color: #1baeae;
+        color: #fff;
+      }
+    }
+    .verify {
+      >div {
+        width: 100%;
+      }
+      display: flex;
+      justify-content: center;
+      .cerify-code-panel {
+        margin-top: 16px;
+      }
+      .verify-code {
+        width: 40%!important;
+        float: left!important;
+      }
+      .verify-code-area {
+        float: left!important;
+        width: 54%!important;
+        margin-left: 14px!important;
+        .varify-input-code {
+          width: 90px;
+          height: 38px!important;
+          border: 1px solid #e9e9e9;
+          padding-left: 10px;
+          font-size: 16px;
+        }
+        .verify-change-area {
+          line-height: 44px;
+        }
+      }
     }
   }
-  .input-icon {
-    height: 39px;
-    width: 14px;
-    margin-left: 2px;
-  }
-}
-.login-tip {
-  font-size: 13px;
-  text-align: center;
-  color: #bfbfbf;
-}
-.login-code {
-  width: 33%;
-  height: 38px;
-  float: right;
-  img {
-    cursor: pointer;
-    vertical-align: middle;
-  }
-}
-.el-login-footer {
-  height: 40px;
-  line-height: 40px;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  text-align: center;
-  color: #fff;
-  font-family: Arial;
-  font-size: 12px;
-  letter-spacing: 1px;
-}
-.login-code-img {
-  height: 38px;
-}
 </style>
